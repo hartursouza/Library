4504f055382aeb1e3db6bbbf32c72315c396287f
Hartur Souza <hartursouza3@gmail.com>
2025-10-08 22:15:06 -0300

    refactor: migrate all styles for template

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,3 +8,49 @@ Alpine.plugin(mask)
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const themeBtn = document.getElementById("themeBtn");
+    const htmlEl = document.documentElement;
+
+    // Função para alternar o tema
+    function toggleTheme() {
+        htmlEl.classList.toggle('dark');
+        const isDarkMode = htmlEl.classList.contains('dark');
+
+        if (themeBtn) {
+            themeBtn.textContent = isDarkMode ? "☀️" : "🌙";
+        }
+
+        localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+    }
+
+    // Adiciona o evento de clique ao botão
+    if (themeBtn) {
+        themeBtn.addEventListener('click', toggleTheme);
+    }
+
+    // Função para carregar o tema salvo ao iniciar a página
+    function loadTheme() {
+        if (localStorage.getItem('theme') === 'dark') {
+            htmlEl.classList.add('dark');
+            if (themeBtn) {
+                themeBtn.textContent = "☀️";
+            }
+        } else {
+            htmlEl.classList.remove('dark');
+             if (themeBtn) {
+                themeBtn.textContent = "🌙";
+            }
+        }
+    }
+
+    loadTheme();
+
+});
+
+import.meta.glob([
+    '../images/**',
+    '../fonts/**',
+]);
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::prefix('/dashboard')->middleware(['auth', 'verified'])->group(function () {
